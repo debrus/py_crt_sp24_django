3dd8c1166676982f54295010643def6bb672747e
Bruce McPeek <seattle_brus@yahoo.com>
2024-06-04 01:19:23 -0700

    Submitting Assignment 08
    Removed "context_object_name" variable per suggestion from
    the "Lesson 08 Assignment" web page
    
    Bruce McPeek
    debrus (github)
    2024sp
    Assignment 08

--- blogging/views.py
+++ blogging/views.py
@@ -7,13 +7,11 @@ from django.views.generic.detail import DetailView
 
 
 class PostListView(ListView):
-    context_object_name = 'post_list'
     queryset = Post.objects.exclude(published_date__exact=None)
     template_name = 'blogging/list.html'
 
 
 class PostDetailView(DetailView):
-    context_object_name = 'post_list'
     queryset = Post.objects.exclude(published_date__exact=None)
     template_name = 'blogging/detail.html'
 
